2562bece65072b31a7165ae731115bd36d5dc01c
radimbig <radimvadim@gmail.com>
2023-07-05 16:14:00 +0200

    Core changes, entity framework changes
    Timespend is not TimeSpan not DateTime

---
+++ TimeManager.Database/Migrations/20230705141213_Init.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeManager.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ObservedProcesses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    OpenedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ClosedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TotalSpent = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ObservedProcesses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ObservedProcesses");
+        }
+    }
+}
--- TimeManager.Database/TimeManagerDbContext.cs
+++ TimeManager.Database/TimeManagerDbContext.cs
@@ -6,12 +6,16 @@ namespace TimeManager.Database
     public class TimeManagerDbContext : DbContext
     {
         private string ConnectionString = "Data Source=TimeManagerDatabase.db";
-        DbSet<ObservedProcess> ObservedProcesses { get; set; }
+        public DbSet<ObservedProcess> ObservedProcesses { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(ConnectionString);
             base.OnConfiguring(optionsBuilder);
+        }
+        public TimeManagerDbContext()
+        {
+            Database.EnsureCreated();
         }
     }
 }
