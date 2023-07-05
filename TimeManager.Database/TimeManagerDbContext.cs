839ad3bbc962a8ae54e318f86cdb9a8149a0a111
radimbig <radimvadim@gmail.com>
2023-07-05 15:23:03 +0200

    Entity framework + SqlLite

---
+++ TimeManager.Database/TimeManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeManager.Core.Models;
+
+namespace TimeManager.Database
+{
+    public class TimeManagerDbContext : DbContext
+    {
+        private string ConnectionString = "Data Source=TimeManagerDatabase.db";
+        DbSet<ObservedProcess> ObservedProcesses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
